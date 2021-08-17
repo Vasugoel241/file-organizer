62a624e4f713354d1d2a0b1bc18ac061cb6e3e8e
Vasu Goel <vasugoel241@gmail.com>
2021-08-17 17:03:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// main input
+//  input -> node main.js tree "path"
+// Print-> tree command executed with path ""
+// Print -> organize command executed with path ""
+//  input -> node main.js help 
+
+ // Print -> list of all the commands
+            // 1. node main.js tree "path"
+            // 2. node main.js organize "path"
+            // 3. node main.js help
+
+
+let helpObj = require("./command/help");
+let treeObj = require("./command/tree");
+let organizeObj = require("./command/organize");
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+switch (command) {
+    case "tree":
+        // node main.js tree "C:\CODING\JavaScript\Module_1_FS"   
+        treeObj.treefxn(inputArr[1]);
+        break;
+        //  input -> node main.js organize "C:\Users\Ritik Singh\Desktop\Batches\PP_12_21\JS\Module_1_FS\randomFolder"
+    case "organize":
+        organizeObj.organizefxn(inputArr[1])
+        break;
+    case "help":
+        helpObj.helpfxn();
+        break;
+    default:
+        console.log("🙏 kindly enter the correct cmd");
+        break;
+}
